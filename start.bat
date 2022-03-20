35a32aeb54a7da8eef8f37bdf738a919fb3acda4
Aristore <hollowayjoseph255@gmail.com>
2022-03-20 13:34:10 +0800

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,12 @@
+@echo off5
+chcp 65001
+cd C:\Users\GY1\Desktop\English
+shutdown -s -t 600
+start 照片1.jpg
+for /L %%a in (
+599,-1,0
+) do (
+ping -n 2 localhost 1>nul 2>nul
+)
+del /f /s /q .\*照片1.jpg*
+PAUSE
